63553ee5995c15a2c789f0c1a09d2fbe6f9f06f3
Ilias Khugaev <big.ilyasus95@gmail.com>
2025-06-01 13:33:34 +0200

    small fixes

--- Week2/Prep-exercise/prep-exercise.sql
+++ Week2/Prep-exercise/prep-exercise.sql
@@ -43,3 +43,24 @@ CREATE TABLE recipe_steps (
   FOREIGN KEY (step_id) REFERENCES steps(step_id)
 );
 
+CREATE TABLE recipe_steps (
+  recipe_id INT,
+  step_id INT,
+  step_order INT,
+  PRIMARY KEY (recipe_id, step_id),
+  FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
+  FOREIGN KEY (step_id) REFERENCES steps(step_id)
+);
+
+CREATE TABLE categories (
+  categories_id INT AUTO_INCREMENT PRIMARY KEY,
+  categories_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE categories_recipes (
+  recipe_id INT,
+  categori_id INT,
+  PRIMARY KEY (recipe_id, categori_id),
+  FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id),
+  FOREIGN KEY (categori_id) REFERENCES categories(categories_id)
+);
