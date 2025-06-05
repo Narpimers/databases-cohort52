e9b734467d5b0bf59172fa60b1f673dbb885e147
Ilias Khugaev <big.ilyasus95@gmail.com>
2025-06-05 16:51:03 +0200

    finale commit

--- Week2/assignment/Keys.js
+++ Week2/assignment/Keys.js
@@ -14,14 +14,21 @@ try {
     CREATE TABLE IF NOT EXISTS authors (
         author_id INT AUTO_INCREMENT PRIMARY KEY,
         author_name VARCHAR(50) NOT NULL,
-        university TEXT,
+        university VARCHAR(50),
         date_of_birth DATE NOT NULL,
         h_index INT NOT NULL,
-        paper_id INT,
         mentor INT,
         gender ENUM("male","female","other"),
         FOREIGN KEY (mentor) REFERENCES authors(author_id)
     )`);
+
+    CREATE TABLE IF NOT EXISTS author_papers (
+        author_id INT,
+        paper_id INT,
+        PRIMARY KEY (author_id, paper_id),
+        FOREIGN KEY (author_id) REFERENCES authors(author_id),
+        FOREIGN KEY (paper_id) REFERENCES research_papers(paper_id)
+    );
 
 
 } catch (error) {
